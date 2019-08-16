9c3f2b953ed37c5bd2d14c9506a96df93d592387
huyihao <yhhutop@163.com>
2019-08-16 16:40:08 +0800

    index update

--- src/main/java/com/dwz/community/controller/IndexController.java
+++ src/main/java/com/dwz/community/controller/IndexController.java
@@ -3,13 +3,11 @@ package com.dwz.community.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/index")
 public class IndexController {
 
-    @GetMapping("/index")
+    @GetMapping("/")
     public String index(){
 
         return "index";
