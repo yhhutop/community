9b9fb121d57a32d53e83f760ec4ac2141d4d42d8
huyihao <yhhutop@163.com>
2019-08-16 18:37:56 +0800

    githup 认证授权

---
+++ src/main/java/com/dwz/community/DTO/AccessTokenDTO.java
@@ -0,0 +1,11 @@
+package com.dwz.community.DTO;
+
+public class AccessTokenDTO {
+
+    private String client_id;
+    private String redirect_uri;
+    private String code;
+    private String client_secret;
+    private String state;
+
+}
