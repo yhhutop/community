d01d3b4d7fbbb5034cb444288d73518586201f33
huyihao <yhhutop@163.com>
2019-08-16 19:08:05 +0800

    githup 认证授权获取Code

--- src/main/java/com/dwz/community/controller/AuthorizeController.java
+++ src/main/java/com/dwz/community/controller/AuthorizeController.java
@@ -1,10 +1,36 @@
 package com.dwz.community.controller;
 
 
+import com.dwz.community.DTO.AccessTokenDTO;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthorizeController {
 
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+    @RequestMapping("/callback")
+    public String callback(@RequestParam("code")String code,
+                           @RequestParam("state")String state){
+
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setState(state);
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+
+        return "redirect:/";
+    }
 
 }
